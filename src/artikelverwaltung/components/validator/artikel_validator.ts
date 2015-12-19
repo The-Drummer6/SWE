c08bb6fc6826ec5ef4529bedff2cbb15459dc7a1
Benjamin Riebs <ribe1012@hs-karlsruhe.de>
2015-12-19 15:59:15 +0100

    validator + Imports

---
+++ src/artikelverwaltung/components/validator/artikel_validator.ts
@@ -0,0 +1,12 @@
+import {Control} from 'angular2/angular2';
+import {isBlank} from '../../../util/util';
+
+export default class ArtikelValidator {
+
+    static name(control: Control): {[key: string]: boolean} {
+
+        const invalid: boolean =
+            isBlank(control.value) || control.value.match(/^\w.*/) === null;
+        return invalid ? {invalidTitel: true} : null;
+    }
+}
